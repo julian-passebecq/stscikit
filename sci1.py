c91ccc3ee27b4ddc356f6693d64f8f0e813b7438
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-17 23:13:32 +0200

    Update sci1.py

--- sci1.py
+++ sci1.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
@@ -18,52 +18,60 @@ def main():
     # Titre de la page
     st.title("Exploration de Data Science et Machine Learning")
     
-    # Sélection du dataset à utiliser
+    # Sélection du dataset
     option = st.selectbox('Quel dataset voulez-vous explorer?', ('Iris', 'Diabetes', 'Wine', 'Breast Cancer'))
     st.write(f"Vous avez sélectionné le dataset {option}")
 
     if option == 'Iris':
-        # Partie 1: Chargement du dataset
+        # Chargement des données
         iris = datasets.load_iris()
         df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
         
-        # Partie 2: Statistique Descriptive
+        # Statistiques descriptives
         st.subheader("Statistique Descriptive")
         st.write(df.describe())
         
-        # Partie 3: EDA (Analyse Exploratoire de Données)
-        st.subheader("Analyse Exploratoire de Données (EDA)")
+        # EDA : Pairplot
+        st.subheader("Analyse Exploratoire de Données (EDA) via Pairplot")
         sns.pairplot(df, hue="target")
         plt.savefig("pairplot.png")
         st.image("pairplot.png")
         
-        # Partie 4: ANOVA (Analyse de Variance)
+        # ANOVA
         st.subheader("Analyse de Variance (ANOVA)")
         fvalue, pvalue = stats.f_oneway(df['sepal length (cm)'], df['sepal width (cm)'], df['petal length (cm)'], df['petal width (cm)'])
         st.write("F-value:", fvalue)
         st.write("P-value:", pvalue)
         
-        # Partie 5: Boxplots groupés
-        st.subheader("Boxplots Groupés")
+        # Boxplots
+        st.subheader("Boxplots pour chaque caractéristique en fonction de la classe")
+        plt.figure(figsize=(12, 6))
         sns.boxplot(x='target', y='sepal length (cm)', data=df)
         plt.savefig("boxplot.png")
         st.image("boxplot.png")
         
-        # Partie 6: Encodage et corrélation
-        st.subheader("Encodage des variables catégorielles et calcul de la corrélation")
+        # Encodage et calcul de la corrélation
+        st.subheader("Encodage des variables catégorielles et corrélation")
         le = LabelEncoder()
         df['target'] = le.fit_transform(df['target'])
         st.write(df.corr())
         
-        # Partie 7: Arbre de décision
+        # Utilisation d'un modèle d'arbre de décision
         st.subheader("Utilisation d'un arbre de décision")
         X = df.drop('target', axis=1)
         y = df['target']
-        clf = DecisionTreeClassifier()
+        clf = DecisionTreeClassifier(random_state=42)
         clf.fit(X, y)
-        st.write('Score:', clf.score(X, y))
+        st.write('Score avec les données d\'entraînement:', clf.score(X, y))
+
+        # Visualisation de l'arbre de décision
+        st.subheader("Visualisation de l'arbre de décision")
+        plt.figure(figsize=(20, 10))
+        plot_tree(clf, filled=True, feature_names=iris['feature_names'], class_names=str(iris['target_names']))
+        plt.savefig("decision_tree.png")
+        st.image("decision_tree.png")
         
-        # Partie 8: Algorithmes de ML
+        # Utilisation de différents algorithmes de ML
         st.subheader("Différents types d'algorithmes de machine learning")
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         lr = LogisticRegression()
