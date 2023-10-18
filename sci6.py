55013c3263c84fd484c479385e32230d598438e8
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-18 17:16:32 +0200

    Update sci6.py

--- sci6.py
+++ sci6.py
@@ -1,45 +1,60 @@
-# Importations
+# Importations nécessaires
 import streamlit as st
 from sklearn import datasets
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
 
-# Fonction principale de l'application Streamlit
+# Informations sur les datasets disponibles
+dataset_info = {
+    'Iris': {'Category': 'Pre-Installed (Toy)', 'Type': 'Classification', 'Uses': 'Pattern recognition', 'Import Method': 'load_iris'},
+    'Diabetes': {'Category': 'Pre-Installed (Toy)', 'Type': 'Regression', 'Uses': 'Medical research', 'Import Method': 'load_diabetes'},
+    'Digits': {'Category': 'Pre-Installed (Toy)', 'Type': 'Classification', 'Uses': 'Handwriting recognition', 'Import Method': 'load_digits'},
+    'Linnerud': {'Category': 'Pre-Installed (Toy)', 'Type': 'Multivariate', 'Uses': 'Physical exercise', 'Import Method': 'load_linnerud'},
+    'Wine': {'Category': 'Pre-Installed (Toy)', 'Type': 'Classification', 'Uses': 'Wine recognition', 'Import Method': 'load_wine'},
+    'Breast Cancer Wisconsin': {'Category': 'Pre-Installed (Toy)', 'Type': 'Classification', 'Uses': 'Cancer diagnosis', 'Import Method': 'load_breast_cancer'},
+    'Olivetti Faces': {'Category': 'Real World', 'Type': 'Classification', 'Uses': 'Face recognition', 'Import Method': 'fetch_olivetti_faces'},
+    '20 Newsgroups': {'Category': 'Real World', 'Type': 'Classification', 'Uses': 'Text categorization', 'Import Method': 'fetch_20newsgroups'},
+    '20 Newsgroups Vectorized': {'Category': 'Real World', 'Type': 'Classification', 'Uses': 'Text categorization', 'Import Method': 'fetch_20newsgroups_vectorized'},
+    'Labeled Faces in the Wild People': {'Category': 'Real World', 'Type': 'Classification', 'Uses': 'Face recognition', 'Import Method': 'fetch_lfw_people'},
+    'Forest Covertypes': {'Category': 'Real World', 'Type': 'Classification', 'Uses': 'Land cover classification', 'Import Method': 'fetch_covtype'},
+    'RCV1': {'Category': 'Real World', 'Type': 'Classification', 'Uses': 'Text categorization', 'Import Method': 'fetch_rcv1'},
+    'Kddcup 99': {'Category': 'Real World', 'Type': 'Classification', 'Uses': 'Network intrusion detection', 'Import Method': 'fetch_kddcup99'},
+    'California Housing': {'Category': 'Real World', 'Type': 'Regression', 'Uses': 'Housing price prediction', 'Import Method': 'fetch_california_housing'}
+}
+
+# Fonction principale
 def main():
-    st.title("Exploration de Data Science avec Pandas Profiling et DataFrame modifiable")
-    st.sidebar.title("Navigation")
+    st.title("Exploration de Data Science avec Streamlit")
 
     # Navigation
-    page = st.sidebar.selectbox("Choisissez une page", ["Introduction", "Statistiques Descriptives", "DataFrame Modifiable", "Conclusion"])
+    page = st.sidebar.selectbox("Choisissez une page", ["Pandas Profiling", "Statistiques Descriptives", "DataFrame Modifiable", "Datasets Scikit-Learn", "Informations Datasets"])
 
-    # Chargement du dataset Iris
-    iris = datasets.load_iris()
-    df = pd.DataFrame(data=iris['data'], columns=iris['feature_names'])
+    if page == "Pandas Profiling":
+        # Code pour Pandas Profiling
+        st.header("Pandas Profiling")
+        # ...
+    elif page == "Statistiques Descriptives":
+        # Code pour Statistiques Descriptives
+        st.header("Statistiques Descriptives")
+        # ...
+    elif page == "DataFrame Modifiable":
+        # Code pour DataFrame Modifiable
+        st.header("DataFrame Modifiable")
+        # ...
+    elif page == "Datasets Scikit-Learn":
+        # Code pour Datasets Scikit-Learn
+        st.header("Datasets Scikit-Learn")
+        # ...
+    elif page == "Informations Datasets":
+        st.header("Informations sur les Datasets de Scikit-Learn")
+        selected_dataset = st.selectbox("Choisissez un dataset pour plus d'informations", list(dataset_info.keys()))
+        st.write("Catégorie :", dataset_info[selected_dataset]['Category'])
+        st.write("Type :", dataset_info[selected_dataset]['Type'])
+        st.write("Utilisations potentielles :", dataset_info[selected_dataset]['Uses'])
+        st.write("Méthode d'importation :", dataset_info[selected_dataset]['Import Method'])
 
-    # Introduction
-    if page == "Introduction":
-        st.subheader("Introduction")
-        st.write("Dans cette application, nous explorerons le dataset Iris avec Pandas Profiling et un DataFrame modifiable.")
-
-    # Statistiques Descriptives
-    if page == "Statistiques Descriptives":
-        st.subheader("Statistiques Descriptives avec Pandas Profiling")
-        report = ProfileReport(df, explorative=True)
-        st_profile_report(report)
-
-    # DataFrame Modifiable
-    if page == "DataFrame Modifiable":
-        st.subheader("DataFrame Modifiable")
-        edited_df = st.experimental_data_editor(df)
-        st.write("DataFrame après modification:")
-        st.dataframe(edited_df)
-
-    # Conclusion
-    if page == "Conclusion":
-        st.subheader("Conclusion")
-        st.write("Nous avons utilisé Pandas Profiling pour l'analyse statistique et st.experimental_data_editor pour la modification du DataFrame.")
-
-# Point d'entrée de l'application
 if __name__ == '__main__':
     main()
