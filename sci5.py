78e9e28aee8c44b07d33e24c37852b70bbfb0a92
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-18 14:43:36 +0200

    Update sci5.py

--- sci5.py
+++ sci5.py
@@ -1,4 +1,4 @@
-# Importation des bibliothèques nécessaires
+# Importation des bibliothèques
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -7,7 +7,6 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
-from scipy import stats
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -16,70 +15,68 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 # Fonction principale de l'application Streamlit
 def main():
+    st.title("Exploration de Data Science et Machine Learning")
     st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Choisir une page", ["Accueil", "Statistiques Descriptives", "Analyse de Variance (ANOVA)", "Corrélation", "Modèles de Machine Learning"])
-
-    if page == "Accueil":
-        st.title("Exploration de Data Science et Machine Learning sur le dataset Iris")
-
-    elif page == "Statistiques Descriptives":
-        st.title("Statistiques Descriptives")
-        # Chargement des données
-        iris = datasets.load_iris()
-        df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
+    page = st.sidebar.selectbox("Choisissez une page", ["Introduction", "Statistiques Descriptives", "Analyse Exploratoire", "ANOVA", "Modèles ML"])
+    
+    if page == "Introduction":
+        st.subheader("Introduction")
+        st.write("Dans cette application, nous explorerons le dataset Iris en utilisant différentes techniques de data science et de machine learning.")
+        
+    # Chargement des données
+    iris = datasets.load_iris()
+    df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
+    if page != "Introduction":
+        st.write(f"Dataset : Iris")
+        
+    if page == "Statistiques Descriptives":
+        st.subheader("Statistiques Descriptives")
         st.write(df.describe())
+        st.subheader("Conclusion")
+        st.write("La description statistique nous donne un aperçu des tendances centrales, de la dispersion et de la forme de la distribution.")
 
-    elif page == "Analyse de Variance (ANOVA)":
-        st.title("Analyse de Variance (ANOVA)")
-        iris = datasets.load_iris()
-        df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
-        # Utilisation d'une heatmap
-        sns.heatmap(df.corr(), annot=True)
+    if page == "Analyse Exploratoire":
+        st.subheader("Analyse Exploratoire de Données (EDA)")
+        sns.pairplot(df, hue="target")
+        plt.savefig("pairplot.png")
+        st.image("pairplot.png")
+        st.subheader("Conclusion")
+        st.write("Le pairplot permet d'identifier les relations entre différentes caractéristiques. Il peut également indiquer les groupes distincts ou les tendances.")
+
+    if page == "ANOVA":
+        st.subheader("Analyse de Variance (ANOVA)")
+        sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
         plt.savefig("heatmap.png")
         st.image("heatmap.png")
-    
-    elif page == "Corrélation":
-        st.title("Corrélation entre les variables")
-        iris = datasets.load_iris()
-        df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
-        # Encodage
-        le = LabelEncoder()
-        df['target'] = le.fit_transform(df['target'])
-        # Heatmap de corrélation
-        sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
-        plt.savefig("correlation.png")
-        st.image("correlation.png")
-        
-    elif page == "Modèles de Machine Learning":
-        st.title("Modèles de Machine Learning")
-        iris = datasets.load_iris()
-        df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
-        # Split des données
+        st.subheader("Conclusion")
+        st.write("L'ANOVA et la heatmap de corrélation nous aident à comprendre l'importance de chaque caractéristique.")
+
+    if page == "Modèles ML":
+        st.subheader("Modèles de Machine Learning")
+        # Division des données
         X = df.drop('target', axis=1)
         y = df['target']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         
         # Forêt aléatoire
-        st.subheader("Forêt aléatoire")
         rf = RandomForestClassifier()
         rf.fit(X_train, y_train)
-        st.write('Score de la forêt aléatoire:', rf.score(X_test, y_test))
-        # Heatmap de la matrice de confusion
-        sns.heatmap(confusion_matrix(y_test, rf.predict(X_test)), annot=True, fmt='d')
-        plt.savefig("confusion_matrix_rf.png")
-        st.image("confusion_matrix_rf.png")
-
+        y_pred_rf = rf.predict(X_test)
+        st.write("Score de la forêt aléatoire:", rf.score(X_test, y_test))
+        st.write(confusion_matrix(y_test, y_pred_rf))
+        st.write(classification_report(y_test, y_pred_rf))
+        
         # Régression logistique
-        st.subheader("Régression logistique")
         lr = LogisticRegression()
         lr.fit(X_train, y_train)
-        st.write('Score de la régression logistique:', lr.score(X_test, y_test))
+        y_pred_lr = lr.predict(X_test)
+        st.write("Score de la régression logistique:", lr.score(X_test, y_test))
+        st.write(confusion_matrix(y_test, y_pred_lr))
+        st.write(classification_report(y_test, y_pred_lr))
 
-        # SVM
-        st.subheader("Machine à vecteurs de support (SVM)")
-        svm = SVC()
-        svm.fit(X_train, y_train)
-        st.write('Score du SVM:', svm.score(X_test, y_test))
+        st.subheader("Conclusion")
+        st.write("Dans cette section, nous avons comparé plusieurs modèles de machine learning pour prédire la classe d'une fleur Iris. Cela permet de comprendre les avantages et les inconvénients de chaque modèle.")
 
+# Point d'entrée de l'application
 if __name__ == '__main__':
     main()
